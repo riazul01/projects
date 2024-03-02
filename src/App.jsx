47946d836ea872b52747282d56d5666f08376d2d
Riazul Islam <riazul5207@gmail.com>
2024-03-02 11:05:15 +0600

    initial setup completed

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="bg-black">
+      <h1 className="text-[#fff]">Hello!</h1>
+    </div>
+  );
+}
+
+export default App;
